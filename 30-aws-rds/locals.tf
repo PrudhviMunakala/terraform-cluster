ef5d6db1edb0d142d4a0753a03e4b369cc5accd5
msrtgit <msrt10.com@gmail.com>
2026-04-22 15:59:50 +0530

    cluster

---
+++ 30-aws-rds/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
+  mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+  database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)
+  tags = {
+    Project     = var.project
+    Environment = var.environment
+    terraform   = "true"
+  }
+}
