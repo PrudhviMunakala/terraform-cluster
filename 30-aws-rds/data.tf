3b90b7a36b05c64eb2082222a839f41f156532a0
msrtgit <msrt10.com@gmail.com>
2026-04-22 16:04:28 +0530

    cluster

--- 30-aws-rds/data.tf
+++ 30-aws-rds/data.tf
@@ -6,10 +6,3 @@ data "aws_ssm_parameter" "mysql_sg_id" {
   name = "/${var.project}/${var.environment}/mysql_sg_id"
 }
 
-data "aws_ssm_parameter" "vpc-id" {
-  name = "/${var.project}/${var.environment}/vpc-id"
-}
-
-data "aws_ssm_parameter" "database_subnet_ids" {
-  name = "/${var.project}/${var.environment}/database_subnet_ids"
-}
--- 30-aws-rds/locals.tf
+++ 30-aws-rds/locals.tf
@@ -1,7 +1,6 @@
 locals {
   database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-  database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)
   tags = {
     Project     = var.project
     Environment = var.environment
